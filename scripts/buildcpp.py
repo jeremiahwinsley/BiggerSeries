9178473b008f90d9dc8e1c3f8a88b160d756b856
RogueLogix <roguelogix@roguelogix.net>
2020-09-18 13:59:06 -0700

    split project moveover

--- scripts/buildcpp.py
+++ scripts/buildcpp.py
@@ -2,7 +2,7 @@ import os.path
 import sys
 
 if __name__ == '__main__':
-    os.chdir("../src/main/quartzpp")
+    os.chdir("../Phosphophyllite/src/main/quartzpp")
     if (os.path.exists("cmake-build-debug")):
         os.chdir("cmake-build-debug")
     else:
@@ -11,9 +11,10 @@ if __name__ == '__main__':
         os.chdir("build")
         os.system("cmake ")
         pass
-    x = os.system("make quartzpp -j")
+    x = os.system("make quartzpp -j ${nproc}")
     if(x != 0):
         sys.exit(2)
+    os.system("mkdir -p ../../resources/assets/phosphophyllite/libs/")
     os.system("cp libquartzpp.so ../../resources/assets/phosphophyllite/libs/")
     os.system("cp ./RogueLib/RogueLib/Exceptions/libRogueLib_Exceptions.so ../../resources/assets/phosphophyllite/libs/libroguelib_exceptions.so")
     os.system("cp ./RogueLib/RogueLib/Threading/libRogueLib_Threading.so ../../resources/assets/phosphophyllite/libs/libroguelib_threading.so")
--- src/main/resources/assets/phosphophyllite/shaders/src/460/render_chunk_query.frag
+++
@@ -1,8 +0,0 @@
-#define VERSION_460
-#include "util/versioning.glsl"
-
-layout(location = 0) out vec4 color;
-
-void main() {
-    color = vec4(1, 1, 1, 1.0);
-}
